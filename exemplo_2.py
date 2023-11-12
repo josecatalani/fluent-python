b44a6ff3af0184eaebe9d63d90ef0fda47728c63
José Victor Catalani <josevictorcatalani@gmail.com>
2023-11-12 19:10:08 -0300

    first commit

---
+++ exemplo_2.py
@@ -0,0 +1,6 @@
+from math import Vector
+
+v1 = Vector(2, 4)
+v2 = Vector(2, 1)
+v3 = v1 + v2
+print(v3)
---
+++ image_example.py
@@ -0,0 +1,9 @@
+import collections
+
+class Image:
+    def __init__(self) -> None:
+        pass
+
+class Carossel:
+    def __init__(self) -> None:
+        self.images = [Image()]
